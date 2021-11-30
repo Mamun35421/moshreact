81ae7d23bdf86544bea464437f16a1b3b0bf59e4
Mamun35421 <mamun35-421@diu.edu.bd>
2021-11-30 12:22:34 +0600

    delete

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -24,6 +24,8 @@ class Counter extends Component {
             <span  className={this.getbgClasses()}>{this.formatCount()}</span>
             <button onClick={() => this.handleIcrement()} className = "btn btn-secondary btn-sm">
                 Increment</button>
+
+                <button className="btn btn-danger btn-sm m-2">Delete</button>
            
         </div>
         );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,8 +5,8 @@ import Counter from './counter';
 class Counters extends React.Component {
     state ={ 
         counters:[
-        {id:1,value: 7},
-        {id:2,value: 3},
+        {id:1,value: 0},
+        {id:2,value: 0},
         {id:3,value: 0},
         {id:4,value: 0},
     ]
