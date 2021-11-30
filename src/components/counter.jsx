2f7a375c2764bf085d690afd1a7394bcddd7be17
Mamun35421 <mamunorrashid.me@gmail.com>
2021-11-30 11:57:28 +0600

    mm

---
+++ src/components/counter.jsx
@@ -0,0 +1,44 @@
+
+import React, { Component } from 'react';
+class Counter extends Component {
+
+    state = {
+        count: this.props.value
+    };
+
+    
+    handleIcrement = product =>{
+        console.log(product);
+        this.setState({count: this.state.count +1});
+       
+        
+    }
+
+  
+    
+    render() { 
+
+       console.log("props",this.props);
+        return (
+        <div>
+            <span  className={this.getbgClasses()}>{this.formatCount()}</span>
+            <button onClick={() => this.handleIcrement()} className = "btn btn-secondary btn-sm">
+                Increment</button>
+           
+        </div>
+        );
+    }
+    getbgClasses(){
+        let classes = "bg m-2 bg-";
+        classes+= this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ?  "Zero" : count;
+    }
+  
+}
+
+ 
+export default Counter ;
---
+++ src/components/counters.jsx
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends React.Component {
+    state ={ 
+        counters:[
+        {id:1,value: 7},
+        {id:2,value: 0},
+        {id:3,value: 0},
+        {id:4,value: 0},
+    ]
+};
+    
+    render() { 
+        return (
+        <div>
+            {this.state.counters.map(counter => 
+            <Counter key={Counter.id} value = {counter.value} selected={true}/>)}
+            
+        </div>
+        );
+    }
+}
+ 
+export default Counters;
