eba5f904be6dbd596b3ca0837df50b91b4a4b0dd
Mamun35421 <mamun35-421@diu.edu.bd>
2021-11-30 13:47:02 +0600

    add

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -25,7 +25,7 @@ class Counter extends Component {
             <button onClick={() => this.handleIcrement()} className = "btn btn-secondary btn-sm">
                 Increment</button>
 
-                <button className="btn btn-danger btn-sm m-2">Delete</button>
+                <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
            
         </div>
         );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,18 +5,24 @@ import Counter from './counter';
 class Counters extends React.Component {
     state ={ 
         counters:[
-        {id:1,value: 0},
+        {id:1,value: 4},
         {id:2,value: 0},
         {id:3,value: 0},
         {id:4,value: 0},
     ]
+};
+handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters});
 };
     
     render() { 
         return (
         <div>
             {this.state.counters.map(counter => 
-            <Counter key={Counter.id} value = {counter.value} selected={true}/>)}
+            <Counter key={Counter.id} onDelete ={this.handleDelete} value = {counter.value} 
+            id={counter.id}/>
+            )}
             
         </div>
         );
